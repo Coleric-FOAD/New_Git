989739d1d483bc31349b1d79f34f895f6027b682
MDevoldere <mdevoldere@users.noreply.github.com>
2020-11-23 13:39:18 +0100

    Ajout script ajout utilisateurs

---
+++ powershell/users/users_import_csv.ps1
@@ -0,0 +1,39 @@
+#Chemin d’accès vers un fichier d’importation CSV
+$ADUsers = Import-csv .\users_list.csv
+
+foreach ($User in $ADUsers)
+{
+
+    $Username    = $User.username
+    $Password    = $User.password
+    $Firstname   = $User.firstname
+    $Lastname    = $User.lastname
+    $Department  = $User.department
+    $OU          = $User.ou
+
+    #Si le compte utilisateur existe déjà dans AD
+    if (Get-ADUser -F {SamAccountName -eq $Username})
+    {
+        #Si l’utilisateur existe: message d’avertissement
+        Write-Warning "User $Username has already exist."
+    }
+    else
+    {
+        #Si l'utilisateur n’existe pas:  nouveau compte utilisateur
+        #Le compte sera créé dans l’unité d’organisation indiquée dans la variable $OU du fichier CSV 
+        New-ADUser `
+        -SamAccountName $Username `
+        -UserPrincipalName "$Username@mondomaine.com" `
+        -Name "$Firstname $Lastname" `
+        -GivenName $Firstname `
+        -Surname $Lastname `
+        -Enabled $True `
+        -ChangePasswordAtLogon $True `
+        -DisplayName "$Lastname, $Firstname" `
+        -Department $Department `
+        -Path $OU `
+        -AccountPassword (convertto-securestring $Password -AsPlainText -Force)
+
+        Write-Host "User $Username added."
+    }
+}
